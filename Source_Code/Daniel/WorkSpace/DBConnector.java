aa886f8e8a527aba9c7b3b48e88df5a5fe3c07f4
dcoroneljr <dcoroneljr@miners.utep.edu>
2016-05-11 00:06:47 -0600

    Added main database subsystem classes

---
+++ Source_Code/Daniel/WorkSpace/DBConnector.java
@@ -0,0 +1,45 @@
+package MPO;
+import java.sql.*;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+public class DBConnector {
+	public Connection connect(String userType) { //Class intended to return a connection to the MPO database.
+		String username;
+		String password;
+		String host = "earth.cs.utep.edu";
+		String dbName = "cs4311team9sp16";
+		Connection conn = null;
+		
+		String [] credentials = credentialProvider(userType);
+		username = credentials[0];
+		password = credentials[1];
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setUser(username);
+		dataSource.setPassword(password);
+		dataSource.setServerName(host);
+		dataSource.setDatabaseName(dbName);
+		dataSource.setPort(3306);
+		try{
+			conn = dataSource.getConnection();
+		}
+		catch(SQLException s){
+			System.out.println("SQL Error: " + s);
+		}
+		return conn;
+	}
+	public String [] credentialProvider(String userType){ //Method used to provide the credentials based on user request
+		switch(userType){
+		case("mpoStaff"):
+			return new String [] {"mpoStaff","Just@notherMPO!"};
+		case("mpoLead"):
+			return new String [] {"mpoLead","ItstimetoLe@d!"};
+		case("projectLead"):
+			return new String [] {"projectLead","!Projecttime!"};
+		case("admin"):
+			return new String [] {"cs4311team9sp16","cs4311!cs4311team9sp16"};
+		default:
+			return new String [] {"guest","LetmevisitMPO!"};
+		}
+	}
+
+}
---
+++ Source_Code/Daniel/WorkSpace/Handler.java
@@ -0,0 +1,10 @@
+package MPO;
+import java.sql.Connection;
+
+public abstract class Handler {
+	public Connection requestConnection(String userType){
+		DBConnector connector = new DBConnector();
+		Connection conn = connector.connect(userType);
+		return conn;
+	}
+}
